b72f95e4c70162d991ff1ce0c9a4ae68ca1c8c1c
Elham <mireshghi@yahoo.com>
2021-11-09 02:10:20 +0000

    finish part 1

--- express_server.js
+++ express_server.js
@@ -34,3 +34,10 @@ app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] /* What goes here? */ };
     res.render("urls_show", templateVars);
   });
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+  function generateRandomString() {
+    return Math.random().toString(36).substr(2, 6);
+}
